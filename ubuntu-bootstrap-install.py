9eb83398a3a08d085b76eada817d76228b734e3e
JT4862 <jt@tensure.io>
2023-12-05 10:51:15 -0600

    fix: add packages list

--- ubuntu-bootstrap-install.py
+++ ubuntu-bootstrap-install.py
@@ -7,17 +7,15 @@ def install_package(package_name):
         print(f"Failed to install {package_name}")
 
 def main():
-    packages = [
-        "awscli", "bash", "bat", "cmake", "colordiff", "ctags", "docker-compose",
-        "docker-machine", "git", "git-flow-avh", "go", "grep", "helm", "hub", "jq",
-        "nmap", "node", "nvm", "pkg-config", "postgresql", "prettier", "rbenv", "redis",
-        "rename", "ripgrep", "rsync", "ruby", "ruby-build", "ruby-install", "ssh-copy-id",
-        "terraform", "thefuck", "vim", "wdiff", "wget", "yarn", "zlib"
-        # Add more package names here
-    ]
+    with open('ubuntu-packages.txt', 'r') as f:
+        packages = [line.strip() for line in f]
 
     for package in packages:
         install_package(package)
+
+def install_package(package):
+    # Your package installation logic here
+    pass
 
 if __name__ == "__main__":
     main()
