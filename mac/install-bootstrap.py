e5b83c9229a55731d887b2ee380d53c675c041b0
JT4862 <jt@tensure.io>
2023-12-05 10:22:44 -0600

    fix:add folder

--- mac/install-bootstrap.py
+++ mac/install-bootstrap.py
@@ -27,7 +27,7 @@ def extract_target(line):
     return None, False
 
 def main():
-    brewfile_path = "Brewfile"  # Path to the Brewfile
+    brewfile_path = "mac/brewfile"  # Path to the Brewfile
     to_install = []
     to_update = []
 
