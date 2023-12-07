6d0a0dbb5a4f8f3b3efa3761cf98ca8b4558083f
JT4862 <jt@tensure.io>
2023-12-07 13:50:16 -0600

    feat: use yum

--- ubuntu-bootstrap-install.py
+++ ubuntu-bootstrap-install.py
@@ -3,7 +3,7 @@ import subprocess
 def install_package(package_name):
     print(f"Installing {package_name}...")
     try:
-        subprocess.run(["sudo", "apt-get", "install", "-y", package_name], check=True)
+        subprocess.run(["sudo", "yum", "install", "-y", package_name], check=True)
         print(f"Successfully installed {package_name}")
         return True
     except subprocess.CalledProcessError:
@@ -11,7 +11,7 @@ def install_package(package_name):
         return False
 
 def main():
-    with open('ubuntu-packages.txt', 'r') as f:
+    with open('centos-packages.txt', 'r') as f:
         packages = [line.strip() for line in f]
 
     successful_installs = []
